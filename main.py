590b36ca1fd74787f45c7ac3dd24356fa9c6e0c4
Clement Dumas <dumasclement2002@gmail.com>
2022-05-02 10:05:10 +0200

    fixed a crash, updated todo and removed statement

--- main.py
+++ main.py
@@ -2,126 +2,12 @@ import sys
 from math import sqrt, cos, sin, pi, ceil, floor
 from random import random, seed
 
-# Game statement for GitHub copilot
-GAME_STATEMENT = """
-# The Goal
-Protect your base from spider attacks and outlive your opponent.
-
-
-# Rules Both players controls a team of 3 heroes. The teams start out at opposite corners of the map, near their 
-base. Throughout the game spiders will appear regularly on the edges of the map. If a spider reaches your base, 
-it will deal damage. If your base takes too much damage, you lose. Thankfully, your heroes can kill spiders. 
-
-
-# The map The game is played on a rectangular map where the coordinate X=0, Y=0 is the top-left pixel and X=17630, 
-Y=9000 is the bottom-right pixel. Fog makes it impossible to know the positions of all spiders and rival heroes. You 
-need to have them within 2200 units from one of your heroes or 6000 from your base. Each base can take a maximum of 3 
-points of damage before being destroyed. Multiple entities (heroes & spiders) can occupy the same coordinates, 
-there is no collision. 
-
-
-# Heroes
-Every turn, you must provide a command for each hero. They may perform any of the following commands:
-WAIT, the hero remains where they are.
-MOVE, followed by map coordinates will make the hero advance towards that point by a maximum of 800 units.
-SPELL, followed by a spell action, as detailed in the Spells section further below.
-Heroes cannot be killed and cannot leave the map.
-After a hero's move phase, any spiders within 800 units will suffer 2 points of damage.
-
-
-# Spiders Every spider appears with a given amount of health. If at the end of a turn, a spider's health has dropped 
-to zero or below, the spider is removed from the game. Spiders will appear randomly, but symmetrically from the map 
-edges outside of the player's bases. They appear with a random moving direction. Spiders will always advance in a 
-straight line at a speed of 400 units per turn. If a spider comes within 5000 units of a base at the end of a turn, 
-it will target that base. When targeting a base, a spider will move directly towards that base and can no longer 
-leave the map. If a spider is pushed (with a WIND command) outside the radius of a targeted base, it will stop 
-targeting and start moving in a randomly selected direction. If a spider comes within 300 units of a base at the end 
-of a turn, as long as it has not been killed on this turn, it will disappear and deal the base 1 point of damage. 
-Each subsequent spider may have slightly more starting health than any previous spider. 
-
-
-# Spells
-Your team will also acquire 1 point of mana per damage dealt to a spider, even from spiders with no health left.
-Mana is shared across the team and heroes can spend 10 mana points to cast a spell.
-A spell command has parameters, which you must separate with a space.
-
-Spell table : command	parameters	| effect | range 
-WIND	<x> <y>	| All entities (except your own heroes) within 1280 units are pushed 2200 units in the direction from 
-the spell caster to x,y. | 1280 
-SHIELD	<entityId> | The target entity cannot be targeted by spells for the next 12 rounds. | 2200 
-CONTROL	<entityId> <x> <y> | Override the target's next action with a step towards the given coordinates. | 2200 
-
-A hero may only cast a spell on entities that are within the spell's range from the hero. 
-
-
-# Victory Conditions
-Your opponent's base health has dropped to zero.
-You have more base health points than your opponent after 220 turns.
-In case of a tie, you have gained the highest amount of wild mana: mana gained outside the radius of your base.
-Defeat Conditions
-Your base's health reaches zero.
-Your program does not provide a valid command in time.
-
-
-# Technical Details After an entity moves towards a point, its coordinates are truncated (when below halfway across 
-the map) or rounded up (when above halfway across the map), only then are distance-based calculations performed (such 
-as spider damage). Spells are cast in the order of the received output. This means a spell may be cancelled if 
-another hero spent the necessary mana earlier in the turn. If an entity is being moved via a CONTROL from multiple 
-sources at once, it will move to the average of all computed destinations. If an entity is being moved via a WIND 
-from multiple sources at once, it will move to the sum of all given directions. SHIELD also protects entities from 
-receiving a new SHIELD. Using a spell against a shielded entity still costs mana. Players are not given the 
-coordinates of spiders outside the map. A spider can be pushed outside of the map, unless it is within a base radius, 
-in which case it will will be moved no further than the border. In case of a tie, the player who gained the highest 
-amount of mana outside the radius of their base will win. this is called wild mana. 
-
-
-# Action order for one turn Wait for both players to output 3 commands. CONTROL spells are applied to the targets and 
-will only be effective on the next turn, after the next batch of commands. SHIELD spells are applied to the targets 
-and will only be effective on the next turn, after the next batch of commands. Does not protect from a spell from 
-this same turn. MOVE all heroes. Heroes attack spiders in range and produce mana for each hit. WIND spells are 
-applied to entities in range. MOVE all spiders according to their current speed, unless they were pushed by a wind on 
-this turn. SHIELD countdowns are decremented. New spiders appear. Dead spiders are removed. 
-
-
-# Game Input/Output Initialization Input Line 1: two integers baseX and baseY for the coordinates of your base. The 
-enemy base will be at the opposite side of the map. Line 2: the integer heroesPerPlayer which is always 3. 
-
-Input for One Game Turn First 2 lines: two integers baseHealth and mana for the remaining health and mana for both 
-players. Your data is always given first. Next line: entityCount for the amount of game entities currently visible to 
-you. Next entityCount lines: 11 integers to describe each entity: id: entity's unique id. type: 0: a spider 1: one of 
-your heroes 2: one of your opponent's heroes x & y: the entity's position. shieldLife: the number of rounds left 
-until entity's shield is no longer active. 0 when no shield is active. isControlled: 1 if this entity is under a 
-CONTROL spell, 0 otherwise. The next 5 integers only apply to spiders (will be -1 for heroes). health: spider's 
-remaining health points. vx & vy: spider's current speed vector, they will add this to their position for their next 
-movement. nearBase: 1: if spider is targeting a base, 0 otherwise. threatFor: With the spider's current trajectory — 
-if nearBase is 0: 0: it will never reach a base. 1: it will eventually reach your base. 2: it will eventually reach 
-your opponent's base. If nearBase is 1: 1 if this spider is targeting your base, 2 otherwise. 
-
-Output for One Game Turn
-3 lines, one for each hero, containing one of the following actions:
-WAIT: the hero does nothing.
-MOVE followed by two integers (x,y): the hero moves 800 towards the given point.
-SPELL followed by a spell command: the hero attempts to cast the given spell.
-You may append text to a command to have it displayed in the viewer above your hero.
-
-Examples:
-MOVE 8000 4500
-SPELL WIND 80 40 casting a wind spell!
-SPELL SHIELD 1
-WAIT nothing to do...
-You must provide a valid command to all heroes each turn, even if they are being controlled by your opponent.
-Constraints
-Response time per turn ≤ 50ms
-Response time for the first turn ≤ 1000ms
-"""
-
 # TODO :
-#  - Don't track 2 times close enemies
-#  - Allow critical defense for tracker
-#  - Update enemy position when they get winded
+#  -? Don't track 2 times close enemies
+#  - Update enemy position when they get winded and increase tolerance to invisible enemies
 #  - Add defensive spell usage :
 #          * SHIELD for attacked heroes
-#          *? SHIELD for secured spiders
+#          *?? SHIELD for secured spiders
 #  - Add offensive spell usage :
 #          * CONTROL to neutralize enemy heroes
 #          *? WIND and CONTROL to send spiders to enemy base
@@ -585,8 +471,11 @@ def patrol(hero, allies):
         return sum(dist(spot, ally) for ally in allies if ally.id != hero.id) - dist(spot, hero) / 2
 
     role = get_role(hero)
-
-    best_spot = max((spot for spot in spots if is_valid_spot(spot, hero, allies)), key=spot_score)
+    valid_spots = [spot for spot in spots if is_valid_spot(spot, hero, allies)]
+    if valid_spots:
+        best_spot = max(valid_spots, key=spot_score)
+    else:
+        best_spot = max(spots, key=spot_score)
     if dist(best_spot, hero) > 2 * HERO_SPEED:
         return best_spot
     init_dir = get_direction(base, best_spot)
